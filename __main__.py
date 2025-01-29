c448493007ba300569524ce5e3546b2bb901f917
Burcak Kam <budemirc@microsoft.com>
2025-01-30 01:54:30 +0300

    Fixed module structure

--- __main__.py
+++ __main__.py
@@ -1,27 +1,14 @@
 import requests
 from dotenv import dotenv_values
 import os
+import FindMoviesSeries.backend.RetrieveInfo as RetrieveInfo
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 secrets = dotenv_values(os.path.join(BASEDIR, '.env'))
 
 def main():
 
-    # url = "https://api.themoviedb.org/3/authentication"
-    url = "https://api.themoviedb.org/3/discover/movie"
-
-    headers = {
-        "accept": "application/json",
-        "Authorization": "Bearer " + secrets["API_BEARER"],
-    }
-
-    response = requests.get(url, headers=headers)
-
-    print(response.text)
-
-    f = open("result.json", "a", encoding="utf-8")
-    f.write(response.text)
-    f.close()
+    RetrieveInfo.retrieveMovies(secrets["API_BEARER"])
 
 
 if __name__ == "__main__":
---
+++ backend/RetrieveInfo.py
@@ -0,0 +1,18 @@
+import requests
+
+def retrieveMovies(bearer):    
+    # url = "https://api.themoviedb.org/3/authentication"
+    url = "https://api.themoviedb.org/3/discover/movie"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer " + bearer,
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
+
+    f = open("result.json", "a", encoding="utf-8")
+    f.write(response.text)
+    f.close()
