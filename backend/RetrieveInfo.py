b33c173e40e06027246f85b11d6687ce8f9c7b8d
Burcak Kam <budemirc@microsoft.com>
2025-01-30 23:40:48 +0300

    Added retrieval of pages

--- backend/RetrieveInfo.py
+++ backend/RetrieveInfo.py
@@ -4,24 +4,35 @@ import FindMoviesSeries.DTO.Movie as MovieClass
 import FindMoviesSeries.DTO.Serie as SerieClass
 
 
-def sendRequest(bearer, url):
-
+def sendRequest(bearer, url, page=0):
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer " + bearer,
     }
 
+    if page != 0:
+        url = url + f"?page={page}"
     response = requests.get(url, headers=headers)
     decoded = json.decoder.JSONDecoder().decode(response.text)
-    print(decoded["total_pages"])
-    print(decoded["total_results"])
-    decoded_results = decoded["results"]
+    return decoded
+
+
+def getAll(bearer, url, maxPage=10):
+    decoded_first = sendRequest(bearer, url)
+    total_page = decoded_first["total_pages"]
+    decoded_results = decoded_first["results"]
+
+    page_range = range(1, maxPage if total_page > maxPage else total_page)
+    for page in page_range:
+        decoded = sendRequest(bearer, url, page)
+        decoded_results_cont = decoded["results"]
+        decoded_results.extend(decoded_results_cont)
 
     return decoded_results
 
 
 def discoverMovies(bearer):
-    results = sendRequest(bearer, "https://api.themoviedb.org/3/discover/movie")
+    results = getAll(bearer, "https://api.themoviedb.org/3/discover/movie")
 
     movies = []
     for movieDict in results:
@@ -32,7 +43,7 @@ def discoverMovies(bearer):
 
 
 def discoverSeries(bearer):
-    results = sendRequest(bearer, "https://api.themoviedb.org/3/discover/tv")
+    results = getAll(bearer, "https://api.themoviedb.org/3/discover/tv")
 
     series = []
     for serieDict in results:
