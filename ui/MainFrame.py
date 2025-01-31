4492269d7e4ad0585d5e090f6d468ff76c7624b1
Burcak Kam <budemirc@microsoft.com>
2025-02-01 01:20:15 +0300

    Added media choice and enabled genre filter

--- ui/MainFrame.py
+++ ui/MainFrame.py
@@ -1,17 +1,16 @@
-from FindMoviesSeries.DTO.Value import Value
+from FindMoviesSeries.DTO.MediaType import MediaType
 import customtkinter
 import tkinter as tk
 from CTkListbox import *
 
-from FindMoviesSeries.backend import RetrieveInfo
 from FindMoviesSeries.ui.DetailFrame import DetailFrame
 
 
 class MainFrame(customtkinter.CTk):
     def getDefaultList(self):
-        movies = RetrieveInfo.discoverMovies(self.bearer, 2)
+        movies = self.infoRetriever.discoverMovies(2)
 
-        self.value = Value.movie
+        self.mediaType = MediaType.movie
         self.valueDict = {}
         i = 0
         for movie in movies:
@@ -19,41 +18,37 @@ class MainFrame(customtkinter.CTk):
             self.listbox.insert(i, movie.title)
             i += 1
 
-    def discoverMoviesButtonClick(self):
-        movies = RetrieveInfo.discoverMovies(self.bearer)
-
-        self.value = Value.movie
+    def discoverButtonClick(self):
+        media = []
         self.valueDict = {}
         i = 0
-        for movie in movies:
-            self.valueDict[movie.title] = movie
-            self.listbox.insert(i, movie.title)
-            i += 1
 
-    def discoverSeriesButtonClick(self):
-        series = RetrieveInfo.discoverSeries(self.bearer)
-
-        self.value = Value.tv
-        self.valueDict = {}
-        i = 0
-        for serie in series:
-            self.valueDict[serie.name] = serie
-            self.listbox.insert(i, serie.name)
-            i += 1
+        if self.mediaType == MediaType.movie:
+            media = self.infoRetriever.discoverMovies(genre_ids=self.selectedGenreIds)
+            for m in media:
+                self.valueDict[m.title] = m
+                self.listbox.insert(i, m.title)
+                i += 1
+        if self.mediaType == MediaType.tv:
+            media = self.infoRetriever.discoverSeries(genre_ids=self.selectedGenreIds)
+            for m in media:
+                self.valueDict[m.name] = m
+                self.listbox.insert(i, m.name)
+                i += 1
 
     def selectListValue(self, selected_option):
         selectedValue = self.valueDict[selected_option]
 
-        if self.value == Value.movie:
+        if self.mediaType == MediaType.movie:
             self.details_frame.setMovieDetails(self.genres, selectedValue)
-        if self.value == Value.tv:
+        if self.mediaType == MediaType.tv:
             self.details_frame.setSerieDetails(self.genres, selectedValue)
 
-    def __init__(self, secrets):
+    def __init__(self, infoRetriever):
         super().__init__()
-        self.bearer = secrets["API_BEARER"]
+        self.infoRetriever = infoRetriever
 
-        self.genres = RetrieveInfo.getMovieGenres(self.bearer)
+        self.genres = self.infoRetriever.getGenres(MediaType.movie)
 
         customtkinter.set_appearance_mode("dark")
         self.title("FindMoviesSeries")
@@ -82,31 +77,43 @@ class MainFrame(customtkinter.CTk):
             self.genres, self.valueDict[list(self.valueDict.keys())[0]]
         )
 
-    def createSidebar(self):
+    def setMediaType(self, choice):
+        self.mediaType = MediaType[choice]
+        self.genres = self.infoRetriever.getGenres(MediaType.movie)
 
+    def setGenre(self, choice):
+        self.selectedGenreIds = [
+            genre.id for genre in self.genres if genre.name == choice
+        ]
+
+    def createSidebar(self):
         self.sidebar_frame = customtkinter.CTkFrame(
             master=self, width=80, corner_radius=0
         )
         self.sidebar_frame.grid(row=0, column=0, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
+        self.mediaTypeOption = customtkinter.CTkOptionMenu(
+            master=self.sidebar_frame,
+            values=[e.name for e in MediaType],
+            command=self.setMediaType,
+        )
+        self.mediaTypeOption.pack(pady=20, padx=10)
+        self.mediaTypeOption.set(MediaType.movie.name)
+
         genreNames = [genre.name for genre in self.genres]
         combobox_var = customtkinter.StringVar(value="Select/Type Genre")
         self.genreComboBox = customtkinter.CTkComboBox(
-            self.sidebar_frame, values=genreNames, variable=combobox_var
+            self.sidebar_frame,
+            values=genreNames,
+            variable=combobox_var,
+            command=self.setGenre,
         )
         self.genreComboBox.pack(pady=20, padx=10)
 
-        self.discoverMoviesButton = customtkinter.CTkButton(
+        self.discoverButton = customtkinter.CTkButton(
             master=self.sidebar_frame,
-            text="Discover movies",
-            command=self.discoverMoviesButtonClick,
+            text="Discover",
+            command=self.discoverButtonClick,
         )
-        self.discoverMoviesButton.pack(pady=20, padx=20)
-
-        self.discoverSeriesButton = customtkinter.CTkButton(
-            master=self.sidebar_frame,
-            text="Discover series",
-            command=self.discoverSeriesButtonClick,
-        )
-        self.discoverSeriesButton.pack(pady=20, padx=20)
+        self.discoverButton.pack(pady=20, padx=20)
